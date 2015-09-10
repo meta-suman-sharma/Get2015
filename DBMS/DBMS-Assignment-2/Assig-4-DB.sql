35d84c6198e5bc13ca36e4bcfabab2f922a1adbd
meta-suman-sharma <suman.sharma@metacube.com>
2015-09-10 18:43:14 +0530

    Update Assig-4-DB.sql

--- DBMS/DBMS-Assignment-2/Assig-4-DB.sql
+++ DBMS/DBMS-Assignment-2/Assig-4-DB.sql
@@ -1,13 +1,16 @@
+/* Create database ZipCode*/
 CREATE DATABASE ZipCode;
 
 USE ZipCode;
 
+/*create table states*/
 CREATE TABLE States(
 State_id VARCHAR(100),
 State_name VARCHAR(100),
 PRIMARY KEY(State_id)
 );
 
+/*create table City*/
 CREATE TABLE City(
 City_id VARCHAR(100),
 City_name VARCHAR(100),
@@ -16,7 +19,7 @@ PRIMARY KEY(City_id),
 FOREIGN KEY(State_id) REFERENCES States(State_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-
+/*create table ZipCode*/
 CREATE TABLE ZipCode(
 Zipcode VARCHAR(100),
 City_id VARCHAR(100),
@@ -24,13 +27,17 @@ PRIMARY KEY(Zipcode),
 FOREIGN KEY(City_id) REFERENCES City(City_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+/*Insert into States*/
 INSERT INTO States VALUES('1','RAJASTHAN'),('2','UP'),('3','GUJRAT');
 
+/*Insert into City*/
 INSERT INTO City VALUES('101','JAIPUR','1'),('102','AGRA','2'),('103','GANDHINAGAR','3');
 
+/*Insert into ZipCode*/
 INSERT INTO ZipCode VALUES('302018','101'),('112012','102'),('000860','103');
  
- 
+/* write a SQL query for that returns a Resultset containing Zip Code,
+City Names and States  ordered by State Name and City Name.*/ 
 SELECT z.ZipCode, c.City_name, s.State_name
 FROM ZipCode z
 INNER JOIN
