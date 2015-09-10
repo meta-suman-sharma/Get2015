1c35ae5b47725e8c8d6bba0f5383209b8a2d6cff
meta-suman-sharma <suman.sharma@metacube.com>
2015-09-10 18:39:38 +0530

    Update Assig-2-Select.sql

--- DBMS/DBMS-Assignment-2/Assig-2-Select.sql
+++ DBMS/DBMS-Assignment-2/Assig-2-Select.sql
@@ -1,5 +1,7 @@
 USE lis;
 
+/*Modify the SELECT command written for Question 9 to display results in ascending 
+order of issue date and within issue date in ascending order of members name.*/
 SELECT i.issue_date,i.accession_no,i.member_id,r.return_date
 FROM book_issue i LEFT JOIN book_return r
 ON i.accession_no=r.accession_no 
